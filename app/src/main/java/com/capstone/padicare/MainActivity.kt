0b73666a813cc7ff7874623611031535a4f6947a
Novriyan <novriyanferdiyanto@gmail.com>
2024-05-28 22:30:50 +0700

    Test UI(Fragment)

--- app/src/main/java/com/capstone/padicare/MainActivity.kt
+++ app/src/main/java/com/capstone/padicare/MainActivity.kt
@@ -6,46 +6,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.capstone.padicare.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        bottomNavigationView = findViewById(R.id.bottomNav)
-        bottomNavigationView.setOnNavigationItemSelectedListener(onBottomNavItemSelectedListener)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.news -> replaceFragment(NewsFragment())
+                R.id.scan -> replaceFragment(ScanFragment())
+                R.id.history -> replaceFragment(HistoryFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+
+                else -> {
+
+                }
+            }
+            true
         }
     }
 
-    private val onBottomNavItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.home -> {
-                startActivity(Intent(this, MainActivity::class.java))
-                true
-            }
-            R.id.news -> {
-                startActivity(Intent(this, NewsActivity::class.java))
-                true
-            }
-            R.id.scan -> {
-                startActivity(Intent(this, ScanActivity::class.java))
-                true
-            }
-            R.id.history -> {
-                startActivity(Intent(this, HistoryActivity::class.java))
-                true
-            }
-            R.id.profile -> {
-                startActivity(Intent(this, ProfileActivity::class.java))
-                true
-            }
-            else -> false
-        }
+    private fun replaceFragment(fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fl_container, fragment)
+        fragmentTransaction.commit()
     }
 }
