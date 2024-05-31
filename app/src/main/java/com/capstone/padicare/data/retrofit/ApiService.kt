e2190bbfce6c2aa5e67ca509b5b79d7d5c8338e5
Novriyan <novriyanferdiyanto@gmail.com>
2024-05-31 19:34:10 +0700

    Fix Bug (Minor)

--- app/src/main/java/com/capstone/padicare/data/retrofit/ApiService.kt
+++ app/src/main/java/com/capstone/padicare/data/retrofit/ApiService.kt
@@ -1,19 +1,13 @@
 package com.capstone.padicare.data.retrofit
 
 import com.capstone.padicare.data.response.LoginResponse
-import com.capstone.padicare.data.response.RegisterResponse
-import okhttp3.MultipartBody
-import okhttp3.RequestBody
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Multipart
 import retrofit2.http.POST
-import retrofit2.http.Part
-import retrofit2.http.Query
+
 
 interface ApiService {
+    /**
     @FormUrlEncoded
     @POST("register")
     suspend fun register(
@@ -21,6 +15,7 @@ interface ApiService {
         @Field("email") email: String,
         @Field("password") password: String
     ): RegisterResponse
+    **/
 
     @FormUrlEncoded
     @POST("login")
