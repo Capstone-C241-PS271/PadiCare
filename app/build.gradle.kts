c5af9000f4a13f2bd468c09f70aee2f551f57811
Rendisaputra33 <gorengmbah@gmail.com>
2024-06-07 15:11:01 +0700

    fix: refactor type response for api call to fixing failed call api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         versionName = "1.0"
 
         buildConfigField("String", "API_KEY", "\"8d82b1ad183d4214ac6f81e96187f288\"")
-        buildConfigField("String","BASE_URL","\"http://192.168.1.8:3000/\"")
+        buildConfigField("String","BASE_URL","\"http://192.168.53.231:8080/\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -54,36 +54,34 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
 
-    implementation ("com.google.android.material:material:1.4.0")
+    implementation (libs.material.v140)
 
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation (libs.androidx.navigation.fragment.ktx)
+    implementation (libs.androidx.navigation.ui.ktx.v260)
 
-    val cameraxVersion = "1.3.0"
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
 
-    implementation("androidx.camera:camera-camera2:$cameraxVersion")
-    implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
-    implementation("androidx.camera:camera-view:$cameraxVersion")
+    implementation (libs.androidx.camera.core)
+    implementation (libs.androidx.camera.camera2.v133)
+    implementation (libs.androidx.camera.lifecycle.v133)
+    implementation (libs.androidx.camera.view.v133)
+    implementation (libs.androidx.camera.extensions)
 
-    implementation ("androidx.camera:camera-core:1.3.3")
-    implementation ("androidx.camera:camera-camera2:1.3.3")
-    implementation ("androidx.camera:camera-lifecycle:1.3.3")
-    implementation ("androidx.camera:camera-view:1.3.3")
-    implementation ("androidx.camera:camera-extensions:1.3.3")
-    val nav_version = "2.5.3" // Check for the latest version
-    implementation ("androidx.navigation:navigation-fragment-ktx:$nav_version")
-    implementation ("androidx.navigation:navigation-ui-ktx:$nav_version")
+    implementation (libs.androidx.navigation.fragment.ktx.v253)
+    implementation (libs.androidx.navigation.ui.ktx.v253)
 
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.activity.ktx)
+    implementation (libs.glide)
 
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
 
 }
