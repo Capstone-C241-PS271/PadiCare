040318837f5dd27e8c7d1244c20a9436752ae440
Novriyan <novriyanferdiyanto@gmail.com>
2024-06-16 13:58:35 +0700

    Full Bottom Sheet

--- app/src/main/java/com/capstone/padicare/data/response/CommentRequest.kt
+++ app/src/main/java/com/capstone/padicare/data/response/CommentRequest.kt
@@ -1,6 +1,5 @@
 package com.capstone.padicare.data.response
 
 data class CommentRequest(
-    val id: Int,
     val content: String
 )
--- app/src/main/java/com/capstone/padicare/ui/feed/CommentsBottomSheet.kt
+++ app/src/main/java/com/capstone/padicare/ui/feed/CommentsBottomSheet.kt
@@ -1,7 +1,9 @@
 package com.capstone.padicare.ui.feed
 
+import android.app.Dialog
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,10 +11,13 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.capstone.padicare.R
 import com.capstone.padicare.data.response.CommentRequest
 import com.capstone.padicare.data.response.CommentResponse
 import com.capstone.padicare.data.retrofit.ApiConfig
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -21,6 +26,13 @@ import retrofit2.Response
 
 class CommentsBottomSheet(private val id: Int) : BottomSheetDialogFragment() {
     private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var dialog: BottomSheetDialog
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        dialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+        return dialog
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,8 +46,8 @@ class CommentsBottomSheet(private val id: Int) : BottomSheetDialogFragment() {
         sendButton.setOnClickListener {
             val content = commentEditText.text.toString()
             if (content.isNotEmpty()) {
-                val comment = CommentRequest(id, content)
-                postComment(comment)
+                val comment = CommentRequest(content)
+                postComment(comment, id)
             } else {
                 Toast.makeText(context, "Comment cannot be empty", Toast.LENGTH_SHORT).show()
             }
@@ -44,7 +56,17 @@ class CommentsBottomSheet(private val id: Int) : BottomSheetDialogFragment() {
         return view
     }
 
-    private fun postComment(comment: CommentRequest) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bottomSheetBehavior = BottomSheetBehavior.from(view.parent as View)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+        val layout = dialog.findViewById<CoordinatorLayout>(R.id.commentLayout)
+        layout?.minimumHeight = Resources.getSystem().displayMetrics.heightPixels
+    }
+
+    private fun postComment(comment: CommentRequest, postId: Int) {
         val apiService = ApiConfig.getApiService()
         sharedPreferences = requireContext().getSharedPreferences("PadiCarePreferences", Context.MODE_PRIVATE)
         var token = sharedPreferences.getString("token", "") ?: ""
@@ -52,7 +74,7 @@ class CommentsBottomSheet(private val id: Int) : BottomSheetDialogFragment() {
 
         CoroutineScope(Dispatchers.IO).launch {
             try {
-                val response: Response<CommentResponse> = apiService.postComment(comment.id, comment, token)
+                val response: Response<CommentResponse> = apiService.postComment(postId, comment, token)
                 activity?.runOnUiThread {
                     if (response.isSuccessful) {
                         Toast.makeText(context, "Comment posted successfully", Toast.LENGTH_SHORT).show()
